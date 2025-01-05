83dbf9e79cae413a89657239780c37d250759857
zxcavn <vlad.azhar2@gmail.com>
2025-01-05 18:13:25 +0300

    refactor: delete TransactionCommission,WalletBalances,XdsCard,XdsNameDropdown,XdsPricingBlock molecules

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,7 +1,3 @@
 export { default as LanguageSelector } from './LanguageSelector';
-export { default as TransactionCommission } from './TransactionCommission';
-export { default as WalletBalances, type BalanceCurrency } from './WalletBalances';
-export { default as XdsCard, type XdsCardProps } from './XdsCard';
-export { default as XdsNameDropdown } from './XdsNameDropdown';
-export { default as XdsPricingBlock, type XdsPricingBlockProps } from './XdsPricingBlock';
 export * from './modals';
+
