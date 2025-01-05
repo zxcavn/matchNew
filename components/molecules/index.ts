abe1d5a050a072b5835f7c0744b74fb757076f60
zxcavn <vlad.azhar2@gmail.com>
2025-01-05 18:10:32 +0300

    refactor: delere NftCard,ProductCard,resourcesBlocks,SearchTokensBlock,SearchXdsNameBlock,SeedPhrase molecules

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,14 +1,7 @@
 export { default as LanguageSelector } from './LanguageSelector';
-export * from './NftCard';
-export { default as ProductCard } from './ProductCard';
-export { default as SearchTokensBlock } from './SearchTokensBlock';
-export { default as SearchXdsNameBlock } from './SearchXdsNameBlock';
-export { default as SeedPhrase } from './SeedPhrase';
 export { default as TransactionCommission } from './TransactionCommission';
 export { default as WalletBalances, type BalanceCurrency } from './WalletBalances';
 export { default as XdsCard, type XdsCardProps } from './XdsCard';
 export { default as XdsNameDropdown } from './XdsNameDropdown';
 export { default as XdsPricingBlock, type XdsPricingBlockProps } from './XdsPricingBlock';
 export * from './modals';
-export * from './resourcesBlocks';
-
--- components/molecules/resourcesBlocks/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ResourcesGetMpxXfi } from './ResourcesGetMpxXfi';
-export { default as ResourcesImageLinks } from './ResourcesImageLinks';
-export { default as ResourcesLink } from './ResourcesLink';
-export { default as ResourcesTitle } from './ResourcesTitle';
