6c2280d340f6c83780ccfa07c166182b3cff5714
zxcavn <vlad.azhar2@gmail.com>
2025-01-06 17:11:33 +0300

    refactor: delete PrimaryNameLabel,ShapeBackground,StakedMpx,TokenAvatar,ValidatorName,Token atoms

--- components/atoms/ValidatorName/index.tsx
+++
@@ -1,1 +0,0 @@
-export { type Props as ValidatorNameProps, default as ValidatorName } from './ValidatorName';
--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,13 +1,3 @@
-
-
-
 export * from './InfoText';
-export { default as PrimaryNameLabel } from './PrimaryNameLabel';
-export { default as ShapeBackground } from './ShapeBackground';
-export { default as StakedMpx } from './StakedMpx';
 export { default as Timer } from './Timer';
-export * from './Token';
-export * from './TokenAvatar';
-export * from './ValidatorName';
 export * from './buttons/LogoutButton';
-
